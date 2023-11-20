f03b5e014f9eb74d0f51aebe3ae6d0e2d5edd808
Puneet <puneety259@gmail.com>
2023-11-20 13:28:47 +0530

    url change in controller reset mail

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -30,7 +30,7 @@ const sendMail = async (email, fullName, token, res) => {
 
             You have requested to reset your password. Please click the following link to reset your password:<br><br>
 
-            <a href="http://localhost:3000/users/resetPassword?token=${token}" class="reset-link">Reset Your Password</a><br><br>
+            <a href="http://localhost:3000/users/forgot-password/resetPassword?token=${token}" class="reset-link">Reset Your Password</a><br><br>
 
             If you did not request a password reset, please ignore this email. Your password will remain unchanged.<br><br>
 
